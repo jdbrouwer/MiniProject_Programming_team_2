96135ea50cffee501cb7e31380bf1b9bcc30d8be
leoje_2l369cz <leo.jenns.s2@gmail.com>
2015-10-12 15:33:54 +0200

    dit is een test bestand

--- xmlfilms.py
+++ xmlfilms.py
@@ -1,5 +1,11 @@
 import codecs
 import requests
+import datetime
+def datum():
+    i = datetime.datetime.now()
+    date = ("%s-%s-%s" % (i.day, i.month, i.year) )
+    print(date)
+    return date
 
 def schrijf_xml(data):
     bestand = open('filmlijst.xml', 'w')
@@ -8,6 +14,13 @@ def schrijf_xml(data):
     bestand.close()
 
 
-response = requests.get('http://www.filmtotaal.nl/api/filmsoptv.xml?apikey=zmp3tnvbezlo4gbl4bh0mkro5e63xzkb&dag=12-10-2015&sorteer=0')
-response.encoding='utf-8'
-schrijf_xml(response.text)
+def apicall():
+    date = datum()
+    response = requests.get('http://www.filmtotaal.nl/api/filmsoptv.xml?apikey=zmp3tnvbezlo4gbl4bh0mkro5e63xzkb&dag='+date+'&sorteer=0')
+    response.encoding='utf-8'
+    schrijf_xml(response.text)
+
+apicall()
+
+
+
