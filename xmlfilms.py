f080870f27835c89196eec26c9f623c422ab8434
jdbrouwer <jdbrouwr@ictmbo.nl>
2015-10-12 14:48:24 +0200

    Database aanvullen met SQL specifieke code

--- xmlfilms.py
+++ xmlfilms.py
@@ -1,14 +1,13 @@
-import urllib.request
-import hjson
 import codecs
-
-url = 'http://www.filmtotaal.nl/api/filmsoptv.xml?apikey=zmp3tnvbezlo4gbl4bh0mkro5e63xzkb&dag=12-10-2015&sorteer=2'
-req = urllib.request.urlopen(url)
-data = hjson.load(req)
+import requests
 
 def schrijf_xml(data):
     bestand = open('filmlijst.xml', 'w')
     bestand = codecs.open('filmlijst.xml', "w", "utf-8")
     bestand.write(str(data))
     bestand.close()
-schrijf_xml(data)
+
+
+response = requests.get('http://www.filmtotaal.nl/api/filmsoptv.xml?apikey=zmp3tnvbezlo4gbl4bh0mkro5e63xzkb&dag=12-10-2015&sorteer=0')
+response.encoding='utf-8'
+schrijf_xml(response.text)
