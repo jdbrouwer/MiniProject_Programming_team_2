d7578d4c802b0e62dd2c0db38f421a7e66832de5
Jochem <jochem.lentz@hotmail.com>
2015-10-12 13:27:04 +0200

    begin database-script

--- database_read.py
+++ database_read.py
@@ -1,7 +1,9 @@
 __author__ = 'Jochem'
 
 import sqlite3
+#het openen van een sql database in /PATH
 sqlite_file = '/Users/Sebastian/Desktop/my_db.sqlite'
+#connect python en sql
 conn = sqlite3.connect(sqlite_file)
 c = conn.cursor()
 
