452f40b07254b58cd253643c64aa27b3f5752a91
jdbrouwer <jdbrouwr@ictmbo.nl>
2015-10-12 14:57:27 +0200

    Database aanvullen met SQL specifieke code

--- xmlfilms.py
+++ xmlfilms.py
@@ -11,3 +11,5 @@ def schrijf_xml(data):
 response = requests.get('http://www.filmtotaal.nl/api/filmsoptv.xml?apikey=zmp3tnvbezlo4gbl4bh0mkro5e63xzkb&dag=12-10-2015&sorteer=0')
 response.encoding='utf-8'
 schrijf_xml(response.text)
+
+print('hello world')
