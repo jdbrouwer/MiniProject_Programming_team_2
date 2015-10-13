2a230d15e103ed20bc3099935f19b7854c8a8bbd
jdbrouwer <jdbrouwr@ictmbo.nl>
2015-10-13 12:02:00 +0200

    expanded for loop function in SQL_Write_Films to allow SQL query to Films table

--- hoofd_file.py
+++ hoofd_file.py
@@ -90,20 +90,18 @@ def SQL_Write_Films(Name_Film,Start,End,Date_of_Film):
     conn = sqlite3.connect(sqlite_file)
     c= conn.cursor()
 
+
     try:
         for e in Film_Name:
             position = Film_Name.index(e)
-            print(Film_Name[position])
-            print(Start_Time[position])
-            print(End_Time[position])
-            print(Date[position])
         #executing sql query for each item in films
             conn.execute('''INSERT INTO Films (Film_Name, Start_time_Film, End_time_Film, Date)
                         VALUES (?,?,?,?)''',(Film_Name[position],Start_Time[position],End_Time[position],Date[position]))
+    except IOError:
+            print("Could not write to database, Check if lists are being passed to this function")
+
     finally:
             conn.commit()
             conn.close()
 
 
-
-SQL_Write_Films(Film_Name,Start_Time,End_Time,Date)
