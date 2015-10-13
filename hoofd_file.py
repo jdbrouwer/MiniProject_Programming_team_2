9309a32c132e17c71be180c52bb48c331bd097fd
Jochem <jochem.lentz@hotmail.com>
2015-10-13 12:03:11 +0200

    begin database-script

--- hoofd_file.py
+++ hoofd_file.py
@@ -1,21 +1,21 @@
-#This is the main file of our python program:
+'''This is the main file of our python program'''
 import sqlite3
 import codecs
 import requests
 import datetime
 import xmltodict
 
-#XML PART
+'''XML PART'''
 
 def datum():
-    #This function specifies the current date, this date is used in the request url
+    '''This function specifies the current date, this date is used in the request url'''
     i = datetime.datetime.now()
     date = ("%s-%s-%s" % (i.day, i.month, i.year) )
     return date
 
 
 def schrijf_xml(data):
-    #This function opens filmlijst.xml and writes it into a local xml file
+    '''This function opens filmlijst.xml and writes it into a local xml file'''
     bestand = open('filmlijst.xml', 'w')
     bestand = codecs.open('filmlijst.xml', "w", "utf-8")
     bestand.write(str(data))
@@ -23,22 +23,22 @@ def schrijf_xml(data):
 
 
 def apicall():
-    #this function transfers data from the filmtotaal server, by using API, into the schrijf_xml function
+    '''this function transfers data from the filmtotaal server, by using API, into the schrijf_xml function'''
     date = datum()
     response = requests.get('http://www.filmtotaal.nl/api/filmsoptv.xml?apikey=zmp3tnvbezlo4gbl4bh0mkro5e63xzkb&dag='+date+'&sorteer=0')
     response.encoding='utf-8'
     schrijf_xml(response.text)
 
 def read_xml():
-    #this function makes it so that the xml file can acctually be read like string
+    '''this function makes it so that the xml file can acctually be read like string'''
     bestand = open('filmlijst.xml', 'r')
     xml_string= bestand.read()
     return xmltodict.parse(xml_string)
 
-#The functions below are used to create lists for titels, begin time and start time
+'''The functions below are used to create lists for titels, begin time and start time'''
 
 def list_titels(lijst):
-    #Here, the titles of the lists are added to a list
+    '''Here, the titles of the lists are added to a list'''
     list = []
     for film in lijst['filmsoptv']['film']:
         list.append(film['titel'])
@@ -54,7 +54,7 @@ def xml_date(lijst):
     return(list)
 
 def list_begin_time(lijst):
-    #The begin times which are listed in the xml files will be put into a list
+    '''The begin times which are listed in the xml files will be put into a list'''
     list = []
     for film in lijst['filmsoptv']['film']:
         bewerk = datetime.datetime.fromtimestamp(
@@ -64,7 +64,7 @@ def list_begin_time(lijst):
     return(list)
 
 def list_end_time(lijst):
-    #The end times which are listed in the xml files will be put into a list
+    '''The end times which are listed in the xml files will be put into a list'''
     list = []
     for film in lijst['filmsoptv']['film']:
         bewerk = datetime.datetime.fromtimestamp(
@@ -79,14 +79,14 @@ Start_Time = list_begin_time(data_xml)
 End_Time = list_end_time(data_xml)
 Date = xml_date(data_xml)
 
-#SQL PART
+'''SQL PART'''
 
 def SQL_Write_Films(Name_Film,Start,End,Date_of_Film):
 
-    #SQLlite database needs to be defined
+    '''SQLlite database needs to be defined'''
     sqlite_file = '..\db_project.sqlite'
 
-    #initializing SQlite connector
+    '''initializing SQlite connector'''
     conn = sqlite3.connect(sqlite_file)
     c= conn.cursor()
 
@@ -97,7 +97,7 @@ def SQL_Write_Films(Name_Film,Start,End,Date_of_Film):
             print(Start_Time[position])
             print(End_Time[position])
             print(Date[position])
-        #executing sql query for each item in films
+            '''executing sql query for each item in films'''
             conn.execute('''INSERT INTO Films (Film_Name, Start_time, End_time, Date)
                         VALUES (?,?,?,?)''',(Film_Name[position],Start_Time[position],End_Time[position],Date[position]))
     finally:
