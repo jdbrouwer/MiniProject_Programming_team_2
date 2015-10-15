cd32479556b4366c0c2687fcac11c6c0c74f208c
leoje_2l369cz <leo.jenns.s2@gmail.com>
2015-10-15 14:18:33 +0200

    code gen_mail_2 truf naar gen_mail

--- hoofd_file.py
+++ hoofd_file.py
@@ -228,15 +228,15 @@ def codegenerator(name, mail, film, starttijd):
         shift = ord(c) + 4
         change = chr(shift)
         gen_name.append(change)
-        gen_name = gen_name[:2] + gen_name[-3:]
-        gen_done_name = ''.join(gen_name)
+        gen_name_2 = gen_name[:2] + gen_name[-3:]
+        gen_done_name = ''.join(gen_name_2)
     gen_mail = []
     for h in mail:
         shift_h = ord(h) + 4
         change_h = chr(shift_h)
         gen_mail.append(change_h)
-        gen_mail = gen_mail[:4] + gen_mail[-4:]
-        gen_done_mail = ''.join(gen_mail_)
+        gen_mail_2 = gen_mail[:4] + gen_mail[-4:]
+        gen_done_mail = ''.join(gen_mail_2)
     e_ticket = gen_done_name +gen_done_mail+ film + starttijd
     print(e_ticket)
     return e_ticket
